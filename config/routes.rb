4c5d4082a7e598ce86ca9094a0afb479b07aa009
Knowden <853172766@qq.com>
2019-11-09 08:42:03 +0800

    add index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'sign#show_sign_in_page'
+  root 'static_page#index'
 
   get  'sign_up',  to: 'sign#show_sign_up_page'
   post 'sign_up',  to: 'sign#do_sign_up'
