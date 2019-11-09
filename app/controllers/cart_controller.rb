9d0e145ba6915801ce91d8451d0b9b0f75565cd6
Knowden <853172766@qq.com>
2019-11-09 23:06:38 +0800

    add create_new_cart function

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -7,11 +7,23 @@ class CartController < ApplicationController
 
   # 接收参数，新建购物车条目
   def new_cart
+    # 减少库存和创建记录应保持事务性，要么都执行，要么都不执行
+    ActiveRecord::Base.transaction do
+      # 减少Item表中的库存
+      item = Item.find_by!(id: cart_params[:item_id])
+      new_amount = item[:amount] - cart_params[:parches_amount].to_i
+      item.update!(amount: new_amount)
 
+      # 创建新的购物车记录到当前用户
+      cart = Cart.new(user_id: session[:current_user]["id"],
+                      item_id: cart_params[:item_id],
+                      amount: cart_params[:parches_amount])
+      cart.save!
+    end
   end
 
   private
   def cart_params
-    params.permit(:parches_amount)
+    params.permit(:parches_amount, :item_id)
   end
 end
