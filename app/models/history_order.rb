af397b4aa793c0f34b54130a85121f695d9c6a8c
Knowden <853172766@qq.com>
2019-11-10 13:58:30 +0800

    remove history_order tb and model

--- app/models/history_order.rb
+++
@@ -1,9 +0,0 @@
-class HistoryOrder < ApplicationRecord
-  belongs_to :user
-  belongs_to :item
-
-  validates :amount, presence: true
-  validates :finish_date, presence: true
-
-  validates :amount, numericality: { greater_than: 0}
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :orders
   has_and_belongs_to_many :payments, through: :user_payments
   has_many :stores
-  has_many :history_orders
   has_many :comments
   has_many :carts
 
