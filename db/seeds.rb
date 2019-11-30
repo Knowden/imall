5625e378ed8a2c363ca545f184f9e569d6aae00b
Knowden <853172766@qq.com>
2019-11-30 21:37:30 +0800

    add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+OrderState.create(name: '待发货')
+OrderState.create(name: '待评价')
+OrderState.create(name: '已完成')
